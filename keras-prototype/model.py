0984d339f0fa558476ff1a9527fd3b3642724031
Dumfarth Felix <f.dumfarth@students.htl-leonding.ac.at>
2021-07-13 11:40:53 +0200

    removed unnecessary imports

--- keras-prototype/model.py
+++ keras-prototype/model.py
@@ -1,7 +1,5 @@
 import json
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
 from tensorflow.keras.preprocessing.text import Tokenizer
