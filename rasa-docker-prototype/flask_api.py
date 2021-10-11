af172b5d8f9d79d04ccbce6157658d36b1677926
StarkaLukas <lstarka141000@gmail.com>
2021-10-11 21:02:02 +0200

    chatbot21-6 - write basic request with flask api

---
+++ rasa-docker-prototype/flask_api.py
@@ -0,0 +1,24 @@
+import requests
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = True
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+def rasa():
+    if request.method == 'POST':
+        id = request.get_json()['id']
+        msg = request.get_json()['msg']
+        url = 'http://localhost:5005/webhooks/rest/webhook'
+
+        data = {'sender': id, 'message': msg}
+
+        return "Running ..."
+
+
+if __name__ == '__main__':
+    app.run()
