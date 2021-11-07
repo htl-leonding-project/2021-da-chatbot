6c6202f5da2b571c2f37258c10fd1a57fdde2abc
Dumfarth-Felix <43465853+Dumfarth-Felix@users.noreply.github.com>
2021-11-07 21:16:58 +0000

    Deploying to rasa-chatbot from  @ 8fe4220c110eaccdb7191c4f3847ac9b64749f0e 🚀

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,7 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
