55b336bde6ee302dc0517cf045abb49c9b2fca0f
starkalukas <43465679+starkalukas@users.noreply.github.com>
2021-11-16 09:06:14 +0000

    Deploying to rasa-chatbot from  @ 3beb1cdef34747ff6c0d00bb3df10bbee2489a9c 🚀

--- actions/actions.py
+++ actions/actions.py
@@ -162,6 +162,8 @@ class ActionNumberOfStudents(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         counter = 0
 
+        dispatcher.utter_message(text=f"Username: {os.getenv('WEBUNTIS_USERNAME'} Password: {os.getenv{'WEBUNTIS_PASSWORD'}")
+
         s.login()
 
         for student in s.students():
