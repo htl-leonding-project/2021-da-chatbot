9f173e6a30c325e40185f4a7c4b21808649ec44e
starkalukas <43465679+starkalukas@users.noreply.github.com>
2022-02-21 20:05:11 +0000

    Deploying to rasa-chatbot from  @ def9973ae1babcb4ad5a813eaa347270362e662e 🚀

--- actions/actions.py
+++ actions/actions.py
@@ -97,49 +97,54 @@ class ActionUtterBranch(Action):
         branch = next(tracker.get_latest_entity_values("branch"), None)
         branch = branch.lower()
 
+        buttons = []
+        buttons.append({"title": 'Welche Programmiersprachen lernt man an der HTL?', "payload": '/programming_languages'})
+        buttons.append({"title": 'Wie komme ich zur HTL?', "payload": '/public_transport_connection'})
+        buttons.append({"title": 'Wie praxisnahe ist der Unterricht in den Werkstätten und Laboren?', "payload": '/workshops_and_labours'})
+        buttons.append({"title": 'Welche anderen Zweige gibt es?', "payload": '/ask_branches'})
 
         if branch:
             if branch == "medientechnik":
                 dispatcher.utter_message(text=f"Natürlich erzähl ich dir etwas über {branch.capitalize()}")
-                dispatcher.utter_message(text="In der Medientechnik lernt man, wie in der Informatik, viel über Programmieren aber auch medientechnische Inhalte, wie Foto- und Videografie, Bildbearbeitung, Audio, Webseitenerstellung oder Mobile Computing kommen hier nicht zu kurz und unterscheiden die Medientechnik von der Informatik.")
                 dispatcher.utter_message(json_message={
                     "branch": "medientechnik"
                 })
+                dispatcher.utter_message(text="In der Medientechnik lernt man, wie in der Informatik, viel über Programmieren aber auch medientechnische Inhalte, wie Foto- und Videografie, Bildbearbeitung, Audio, Webseitenerstellung oder Mobile Computing kommen hier nicht zu kurz und unterscheiden die Medientechnik von der Informatik.", buttons=buttons)
             elif branch == "informatik":
                 dispatcher.utter_message(text=f"Natürlich erzähl ich dir etwas über {branch.capitalize()}")
-                dispatcher.utter_message(text="Die Fachrichtung vereint eine EDV-technische und betriebswirtschaftliche Ausbildung. Diese Kombination bietet eine umfangreiche, praxisnahe Ausbildung, die später im Beruf direkt eingesetzt werden kann.")
                 dispatcher.utter_message(json_message={
                     "branch": "informatik"
                 })
+                dispatcher.utter_message(text="Die Fachrichtung vereint eine EDV-technische und betriebswirtschaftliche Ausbildung. Diese Kombination bietet eine umfangreiche, praxisnahe Ausbildung, die später im Beruf direkt eingesetzt werden kann.", buttons=buttons)
             elif branch == "elektronik":
                 dispatcher.utter_message(text=f"Natürlich erzähl ich dir etwas über {branch.capitalize()}")
-                dispatcher.utter_message(text="Die vielseitige Ausbildung vereint Hardware und Software in nahezu unendlich vielen Anwendungen. Bis zur Matura sind Schüler und Schülerinnen in der Lage, umfangreichere elektronische Schaltungen und Systeme aufzubauen und zu programmieren.")
                 dispatcher.utter_message(json_message={
                     "branch": "elektronik"
                 })
+                dispatcher.utter_message(text="Die vielseitige Ausbildung vereint Hardware und Software in nahezu unendlich vielen Anwendungen. Bis zur Matura sind Schüler und Schülerinnen in der Lage, umfangreichere elektronische Schaltungen und Systeme aufzubauen und zu programmieren.", buttons=buttons)
             elif branch == "medizintechnik":
                 dispatcher.utter_message(text=f"Natürlich erzähl ich dir etwas über {branch.capitalize()}")
-                dispatcher.utter_message(text="In der Medizintechnik ist Elektronik eines der wichtigsten Elemente und wird daher in der Ausbildung entsprechend behandelt. Zugleich erfolgt eine Einführung in medizinische Themenbereiche wie Anatomie und Physiologie sowie Biosignalverarbeitung und Medizin- und Gesundheitsinformatik.")
                 dispatcher.utter_message(json_message={
                     "branch": "medizintechnik"
                 })
+                dispatcher.utter_message(text="In der Medizintechnik ist Elektronik eines der wichtigsten Elemente und wird daher in der Ausbildung entsprechend behandelt. Zugleich erfolgt eine Einführung in medizinische Themenbereiche wie Anatomie und Physiologie sowie Biosignalverarbeitung und Medizin- und Gesundheitsinformatik.", buttons=buttons)
             elif branch == "fachschule":
                 dispatcher.utter_message(text=f"Natürlich erzähl ich dir etwas über {branch.capitalize()}")
-                dispatcher.utter_message(text="Im fachpraktischen Unterricht wird fachtheoretisches Wissen gefestigt und durch die Herstellung verschiedener Werkstücke mit dem Erlernen von praktischen Fertigkeiten ergänzt. Die 4-jährige Fachschulausbildung unterscheidet sich u.a. zur 5-jährigen HTL-Ausbildung auch durch den höheren Praxisanteil an Unterrichtsstunden.")
                 dispatcher.utter_message(json_message={
                     "branch": "null"
                 })
+                dispatcher.utter_message(text="Im fachpraktischen Unterricht wird fachtheoretisches Wissen gefestigt und durch die Herstellung verschiedener Werkstücke mit dem Erlernen von praktischen Fertigkeiten ergänzt. Die 4-jährige Fachschulausbildung unterscheidet sich u.a. zur 5-jährigen HTL-Ausbildung auch durch den höheren Praxisanteil an Unterrichtsstunden.", buttons=buttons)
             elif branch == "abendschule":
                 dispatcher.utter_message(text=f"Natürlich erzähl ich dir etwas über {branch.capitalize()}")
-                dispatcher.utter_message(text="Seit dem Schuljahr 2015/16 gilt der neue Lehrplan für Informatik an unserer Abendschule. Dieser Lehrplan sieht die Konzentration der Ausbildung auf Bildungsinhalte der Informatik vor. Als schulautonomer Schwerpunkt wird in Leonding Software-Engineering angeboten.")
                 dispatcher.utter_message(json_message={
                     "branch": "null"
                 })
+                dispatcher.utter_message(text="Seit dem Schuljahr 2015/16 gilt der neue Lehrplan für Informatik an unserer Abendschule. Dieser Lehrplan sieht die Konzentration der Ausbildung auf Bildungsinhalte der Informatik vor. Als schulautonomer Schwerpunkt wird in Leonding Software-Engineering angeboten.", buttons=buttons)
             else:
-                dispatcher.utter_message(text=f"Die Fachrichtung {branch} kenne ich leider nicht.")
                 dispatcher.utter_message(json_message={
                     "branch": "null"
                 })
+                dispatcher.utter_message(text=f"Die Fachrichtung {branch} kenne ich leider nicht.", buttons=buttons)
 
         return []
 
@@ -264,13 +269,19 @@ class ActionWhatTimeIsIt(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        buttons = []
+
+        buttons.append({"title": 'Welcher Tag ist heute?', "payload": '/what_date_is_it'})
+        buttons.append({"title": 'Willst du mich heiraten?', "payload": 'Willst du mich heiraten?'})
+        buttons.append({"title": 'Kannst du mir etwas über die HTL erzählen?', "payload": '/ask_htl_general'})
+        buttons.append({"title": 'Wer hat dich gemacht?', "payload": 'Wer hat dich gemacht?'})
 
         now = datetime.now()
 
         current_time = now.strftime("%H:%M:%S")
         print("Current Time =", current_time)
 
-        dispatcher.utter_message(f"Es ist gerade {current_time} Uhr!")
+        dispatcher.utter_message(text=f"Es ist gerade {current_time} Uhr!", buttons=buttons)
 
         return []
 
@@ -282,6 +293,13 @@ class ActionWhatDateIsIt(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        buttons = []
+
+        buttons.append({"title": 'Wie spät ist es?', "payload": '/what_time_is_it'})
+        buttons.append({"title": 'Willst du mich heiraten?', "payload": 'Willst du mich heiraten?'})
+        buttons.append({"title": 'Kannst du mir etwas über die HTL erzählen?', "payload": '/ask_htl_general'})
+        buttons.append({"title": 'Wer hat dich gemacht?', "payload": 'Wer hat dich gemacht?'})
+
 
         now = datetime.now()
         weekday = datetime.today().weekday()
@@ -290,7 +308,7 @@ class ActionWhatDateIsIt(Action):
         current_date = now.strftime("%d.%m.%Y")
         print("Current Date =", current_date)
 
-        dispatcher.utter_message(f"Heute ist {weekday_string} der {current_date}!")
+        dispatcher.utter_message(text=f"Heute ist {weekday_string} der {current_date}!", buttons=buttons)
 
         return []
 
